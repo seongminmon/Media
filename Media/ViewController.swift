8aa29e86a33fed1bc17e87ecb94ad9f8e977faf0
seongmin kim <k2417000@naver.com>
2024-06-24 14:19:48 +0900

    Initial Commit

---
+++ Media/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by 김성민 on 6/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
