95c894eb3e580c2094d4d0125508e827c33fb79f
seongmin kim <k2417000@naver.com>
2024-06-27 09:12:20 +0900

    URLSession으로 변경해보기

--- Media/Network/NetworkManager.swift
+++ Media/Network/NetworkManager.swift
@@ -29,6 +29,7 @@ class NetworkManager {
                 
             case .failure(let error):
                 print("trending ERROR")
+                print(error)
                 completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
@@ -49,6 +50,7 @@ class NetworkManager {
                 
             case .failure(let error):
                 print("credit ERROR")
+                print(error)
                 completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
@@ -69,6 +71,7 @@ class NetworkManager {
                 
             case .failure(let error):
                 print("search ERROR")
+                print(error)
                 completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
@@ -89,6 +92,7 @@ class NetworkManager {
                 
             case .failure(let error):
                 print("similar ERROR")
+                print(error)
                 completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
@@ -109,29 +113,55 @@ class NetworkManager {
                 
             case .failure(let error):
                 print("recommend ERROR")
+                print(error)
                 completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
     }
 
-    func poster(api: NetworkRequest, completionHandler: @escaping (ImageResponse?, String?) -> Void) {
-        AF.request(api.endpoint,
-                   method: api.method,
-                   parameters: api.parameters,
-                   encoding: api.encoding,
-                   headers: api.header)
-        .validate(statusCode: 200..<500)
-        .responseDecodable(of: ImageResponse.self) { response in
-            switch response.result {
-            case .success(let value):
+//    func poster(api: NetworkRequest, completionHandler: @escaping (ImageResponse?, String?) -> Void) {
+//        AF.request(api.endpoint,
+//                   method: api.method,
+//                   parameters: api.parameters,
+//                   encoding: api.encoding,
+//                   headers: api.header)
+//        .validate(statusCode: 200..<500)
+//        .responseDecodable(of: ImageResponse.self) { response in
+//            switch response.result {
+//            case .success(let value):
+//                print("poster SUCCESS")
+//                completionHandler(value, nil)
+//                
+//            case .failure(let error):
+//                print("poster ERROR")
+//                print(error)
+//                completionHandler(nil, "잠시 후 다시 시도해주세요.")
+//            }
+//        }
+//    }
+
+    // MARK: - URLSession으로 변경해보기
+    func poster(
+        api: NetworkRequest,
+        completionHandler: @escaping (ImageResponse?, String?) -> Void
+    ) {
+        let url = api.endpoint
+        var request = URLRequest(url: url, timeoutInterval: 5)
+        request.httpMethod = api.method.rawValue
+        request.headers = api.header
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // response
+            if let error = error {
+                print("poster ERROR")
+                print(error)
+                completionHandler(nil, "잠시 후 다시 시도해주세요.")
+            } else if let data = data {
+                let value = try? JSONDecoder().decode(ImageResponse.self, from: data)
                 print("poster SUCCESS")
                 completionHandler(value, nil)
-                
-            case .failure(let error):
-                print("poster ERROR")
-                completionHandler(nil, "잠시 후 다시 시도해주세요.")
             }
         }
+        .resume()
     }
-    
 }
