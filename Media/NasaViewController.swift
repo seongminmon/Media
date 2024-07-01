d65e04fe0dfde9d9ba24c6bb3cb85920a73e5180
seongmin kim <k2417000@naver.com>
2024-07-01 17:03:47 +0900

    Nasa Image 네트워크 통신 구현

--- Media/NasaViewController.swift
+++ Media/NasaViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 import SnapKit
 
+private enum Nasa: String, CaseIterable {
+    
+    static let baseURL = "https://apod.nasa.gov/apod/image/"
+    
+    case one = "2308/sombrero_spitzer_3000.jpg"
+    case two = "2212/NGC1365-CDK24-CDK17.jpg"
+    case three = "2307/M64Hubble.jpg"
+    case four = "2306/BeyondEarth_Unknown_3000.jpg"
+    case five = "2307/NGC6559_Block_1311.jpg"
+    case six = "2304/OlympusMons_MarsExpress_6000.jpg"
+    case seven = "2305/pia23122c-16.jpg"
+    case eight = "2308/SunMonster_Wenz_960.jpg"
+    case nine = "2307/AldrinVisor_Apollo11_4096.jpg"
+     
+    static var photo: URL {
+        return URL(string: Nasa.baseURL + Nasa.allCases.randomElement()!.rawValue)!
+    }
+}
+
 final class NasaViewController: BaseViewController {
     
     let nasaImageView = UIImageView()
     let progressLabel = UILabel()
     let requestButton = UIButton()
+    
+    var session: URLSession!
+    
+    var total: Double = 0
+    var buffer: Data? {
+        didSet {
+            let result = String(format: "%.2f", Double(buffer?.count ?? 0) / total * 100)
+            progressLabel.text = "\(result) %"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +94,53 @@ final class NasaViewController: BaseViewController {
     
     @objc func requestButtonClicked() {
         print(#function)
+        requestButton.isEnabled = false
+        nasaImageView.image = UIImage(systemName: "star")
+        buffer = Data()
+        callRequest()
     }
     
+    func callRequest() {
+        let request = URLRequest(url: Nasa.photo)
+        session = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
+        session.dataTask(with: request).resume()
+    }
+}
+
+extension NasaViewController: URLSessionDataDelegate {
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse) async -> URLSession.ResponseDisposition {
+//        print(#function)
+        
+        guard let response = response as? HTTPURLResponse,
+              (200..<300).contains(response.statusCode) else {
+            print("응답이 잘못되었습니다.")
+            return .cancel
+        }
+        
+        guard let value = response.value(forHTTPHeaderField: "Content-Length"),
+              let contentLength = Double(value) else {
+            print("Content-Length 키 값이 올바르지 않습니다.")
+            return .cancel
+        }
+        
+        print("성공")
+        total = contentLength
+        return .allow
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+//        print(#function)
+        buffer?.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
+//        print(#function)
+        if error == nil {
+            progressLabel.text = "다운로드가 완료되었습니다."
+            nasaImageView.image = UIImage(data: buffer!)
+        } else {
+            progressLabel.text = "문제가 발생했습니다."
+        }
+        requestButton.isEnabled = true
+    }
 }
